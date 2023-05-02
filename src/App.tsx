ac5898f4b05f54a8631e34b1b641592db965da84
prasetyoow <praztamer@gmail.com>
2023-05-02 19:28:10 +0700

    useContext strict typing with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,14 @@
 import './App.css';
-import { Container } from './components/Container';
-// import { Button } from './components/Button';
-// import { Input } from './components/Input';
-// import { Grammy } from './components/Grammy';
-// import { Greet } from './components/Greet';
-// import { Heading } from './components/Heading';
-// // import { Person } from './components/Person';
-// // import { PersonList } from './components/PersonList';
-// import { Status } from './components/Status';
+import { UserContextProvider } from './components/context/UserContext';
+import { User } from './components/context/User';
 
-// function App() {
-//     const personName = {
-//       first: "Clark",
-//       last: "Kent",
-//     }
-
-//     const nameList = [
-//       {
-//         first: "kantoi",
-//         last: "jeje"
-//       },
-//       {
-//         first: "bruce",
-//         last: "teeth"
-//       },
-//       {
-//         first: "jamal",
-//         last: "kinan"
-//       },
-//     ]
-
-//     return (
-//     <div className="App">
-//       <Greet name="Jun" messageCount={20} isLoggedIn={false}/>
-//       <Person name={personName} />
-//       <PersonList names={nameList}/>
-
-//     </div>
-//   );
-// }
-
-// function App () {
-//   return (
-//     <div className='App'>
-//       <Status status='error'/>
-//       <Heading>Placeholder text</Heading>
-//       <Grammy>
-//         <Heading>Leonardo Dcaprio</Heading>
-//       </Grammy>
-//       <Greet name="jun" messageCount={10} isLoggedIn={true}/>
-//     </div>
-//   )
-// }
-
-// function App() { 
-//   return (
-//     <div className='App'>
-//       <Button handleClick={(event) => {
-//         console.log("Button clicked", event)
-//       }}/>
-//       <Input value="" handleChange={(e) => console.log(e)} />
-//     </div>
-//   )
-// }
 
 function App() {
   return (
     <div className='App'>
-      <Container styles={{border: '1px solid black', padding: "1rem", display: "flex"}} />
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
     </div>
   )
 }
