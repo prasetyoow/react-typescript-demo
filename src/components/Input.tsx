d20a155d342653fbdd41ca9d09114ce8ab07538e
prasetyoow <praztamer@gmail.com>
2023-05-02 10:37:22 +0700

    destructuring props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,10 +5,10 @@ type InputProps = {
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input = (props: InputProps) => {
+export const Input = ({value, handleChange}: InputProps) => {
   // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
   //   console.log(e)
   // }
   
-  return <input type="text" value={props.value} onChange={props.handleChange}/>
+  return <input type="text" value={value} onChange={handleChange}/>
 }
