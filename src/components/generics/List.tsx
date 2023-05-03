69937dfec23bb084082f3864f1515b658f4d8ff5
prasetyoow <praztamer@gmail.com>
2023-05-03 12:17:09 +0700

    generic props strict typing

---
+++ src/components/generics/List.tsx
@@ -0,0 +1,17 @@
+type ListProps = {
+  items: string[] | number[] | {}[]
+  onClick: (value: string | number | object) => void
+}
+
+export const List = ({items, onClick}: ListProps) => {
+  return (
+    <div>
+      <h2>List of items</h2>
+      {items.map((item, index) => {
+        return (
+          <div key={index} onClick={()=> onClick(item)}> {item}</div>
+        )
+      })}
+    </div>
+  )
+}
