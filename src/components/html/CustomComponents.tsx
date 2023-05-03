6765c0f59a2da10d3e6062a102d37bc2a7ce4a5a
prasetyoow <praztamer@gmail.com>
2023-05-03 16:58:45 +0700

    extracting a component props

---
+++ src/components/html/CustomComponents.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Greet } from '../Greet'
+
+export const CustomComponents = (props: React.Component<typeof Greet>) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
